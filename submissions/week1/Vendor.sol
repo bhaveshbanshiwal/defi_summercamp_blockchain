a49387b8b052da9aecc58eeaed28bce9e2c66374
Bhavesh Banshiwal <97866170+bhaveshbanshiwal@users.noreply.github.com>
2026-06-14 15:45:00 +0530

    Fix Vendor math and add basic Flask interface

--- submissions/week1/Vendor.sol
+++ submissions/week1/Vendor.sol
@@ -8,7 +8,14 @@ contract Vendor {
         yourToken = YourToken(tokenAddress);
     }
     function buyTokens() public payable {
-        uint256 amountOfTokens = 100;
+        uint256 amountOfTokens = msg.value * tokensPerEth;
         yourToken.transfer(msg.sender, amountOfTokens);
+    }
+    function sellTokens(uint256 amount) public {
+        require(yourToken.balanceOf(msg.sender) >= amount, "Not enough tokens");
+        uint256 ethAmount = amount / tokensPerEth;
+        yourToken.transferFrom(msg.sender, address(this), amount);
+        (bool sent, ) = msg.sender.call{value: ethAmount}("");
+        require(sent, "Failed to send ETH");
     }
 }

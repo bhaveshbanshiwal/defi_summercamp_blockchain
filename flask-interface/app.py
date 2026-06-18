0ca859b15389cd3e842cb8aa83f9055b9f158cf8
Bhavesh Banshiwal <97866170+bhaveshbanshiwal@users.noreply.github.com>
2026-06-18 12:00:22 +0530

    Remove unused global constants in Flask app

--- flask-interface/app.py
+++ flask-interface/app.py
@@ -2,8 +2,7 @@ from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
-DEX_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
-BALLOONS_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+
 
 @app.route('/')
 def index():
